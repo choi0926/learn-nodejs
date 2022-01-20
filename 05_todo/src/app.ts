25411bee24023bc4ca8a22fe8bc58e8d9bfd6f6c
choi0926 <ghtjd753753@gmail.com>
2022-01-20 22:16:33 +0900

    update todo

--- 05_todo/src/app.ts
+++ 05_todo/src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import { nanoid } from 'nanoid';
 
 interface ITodo {
   id: string;
@@ -15,13 +16,85 @@ const app = express();
 app.set('port', 3000);
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
-
 app.use(
   express.static(path.join(__dirname, 'public'), { extensions: ['html'] })
 );
+// req값을 가져올때 사용하는 미들웨어
+app.use(express.json());
 
 app.get('/api/todos', (req, res) => {
   return res.json({ isSuccess: true, todos });
+});
+
+interface ICreateTodoRequest extends Request {
+  body: {
+    content: string;
+  };
+}
+
+app.post('/api/todos', (req: ICreateTodoRequest, res) => {
+  const { content } = req.body;
+
+  if (!content && typeof content !== 'string') {
+    return res.json({ isSuccess: false });
+  }
+
+  const todo: ITodo = { id: nanoid(), content, isComplete: false };
+  todos.push(todo);
+
+  return res.json({ isSuccess: true, todo });
+});
+
+interface IUpdateTodoRequest extends Request {
+  params: {
+    id: string;
+  };
+  body: {
+    isComplete: boolean;
+  };
+}
+
+// params는 미들웨어가 필요없음
+app.put('/api/todos/:id', (req: IUpdateTodoRequest, res) => {
+  const { id } = req.params;
+  const { isComplete } = req.body;
+
+  if (!id || isComplete === undefined) {
+    return res.json({ isSuccess: false });
+  }
+
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index === -1) {
+    return res.json({ isSuccess: false });
+  }
+
+  todos[index].isComplete = isComplete;
+
+  console.log(isComplete);
+  return res.json({ isSuccess: true });
+});
+
+interface IDeleteTodoRequest extends Request {
+  params: {
+    id: string;
+  };
+}
+
+app.delete('/api/todos/:id', (req: IDeleteTodoRequest, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.json({ isSuccess: false });
+  }
+
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index === -1) {
+    return res.json({ isSuccess: false });
+  }
+
+  todos.splice(index, 1);
+  // index 위치에서 1개 삭제
+
+  return res.json({ isSuccess: true });
 });
 
 app.use((req, res) => {
--- 05_todo/src/public/script/app.js
+++ 05_todo/src/public/script/app.js
@@ -19,6 +19,32 @@ const createTodo = (todo) => {
   span.innerText = todo.content;
   button.innerText = 'Delete';
 
+  checkbox.addEventListener('click', async (event) => {
+    try {
+      const isComplete = event.target.checked;
+      const { data } = await axios.put(`/api/todos/${todo.id}`, { isComplete });
+      if (data.isSuccess) {
+        const index = todos.findIndex(({ id }) => id === todo.id);
+        todos[index].isComplete = isComplete;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  button.addEventListener('click', async () => {
+    try {
+      const { data } = await axios.delete(`/api/todos/${todo.id}`);
+      if (data.isSuccess) {
+        const index = todos.findIndex(({ id }) => id === todo.id);
+        todos.splice(index, 1);
+        ul.removeChild(li);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
   li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(button);
@@ -26,11 +52,20 @@ const createTodo = (todo) => {
 };
 
 // form 태그 사용시 preventDefault 추가
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+form.addEventListener('submit', async (event) => {
+  try {
+    event.preventDefault();
 
-  console.log(input.value);
-  input.value = '';
+    const { data } = await axios.post('/api/todos', { content: input.value });
+
+    if (data.isSuccess) {
+      createTodo(data.todo);
+
+      input.value = '';
+    }
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 const app = async () => {
